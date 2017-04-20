8e4de9e0925f9d9a004337cff6d0f0e189ec4366
Stefan Gasterstädt <gasterstaedt@ehealthexperts.de>
2017-04-20 17:19:06 +0200

    Nice, but not necessary

--- plugin/src/main/java/org/j8unit/maven/plugin/util/CheckedFunction.java
+++ plugin/src/main/java/org/j8unit/maven/plugin/util/CheckedFunction.java
@@ -1,6 +1,5 @@
 package org.j8unit.maven.plugin.util;
 
-import static java.util.Objects.requireNonNull;
 import java.util.function.Function;
 
 @FunctionalInterface
@@ -25,28 +24,6 @@ extends Function<T, R> {
     public static <R, E extends Exception> R hiddenThrow(final Exception any)
     throws E {
         throw (E) any;
-    }
-
-    @Override
-    default <V> CheckedFunction<V, R> compose(final Function<? super V, ? extends T> before) {
-        requireNonNull(before);
-        return (final V v) -> apply(before.apply(v));
-    }
-
-    default <V> CheckedFunction<V, R> compose(final CheckedFunction<? super V, ? extends T> before) {
-        requireNonNull(before);
-        return (final V v) -> apply(before.apply(v));
-    }
-
-    @Override
-    default <V> CheckedFunction<T, V> andThen(final Function<? super R, ? extends V> after) {
-        requireNonNull(after);
-        return (final T t) -> after.apply(apply(t));
-    }
-
-    default <V> CheckedFunction<T, V> andThen(final CheckedFunction<? super R, ? extends V> after) {
-        requireNonNull(after);
-        return (final T t) -> after.apply(apply(t));
     }
 
     public static <T, R> CheckedFunction<T, R> doIt(final CheckedFunction<T, R> code) {
